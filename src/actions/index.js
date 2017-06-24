1d479e9952d8b82a2e778641a305f0ceb3993b20
Jack Borrebach <jborrebach@gmail.com>
2017-06-24 17:48:43 -0400

    Created fetchWeather action,
    which calls openweathermap API. Also connected this action to searchBar container

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,2 +1,20 @@
-const API_KEY = '83a5f58152c31776f45da4292b4ac3b4';
+import axios from 'axios';
 
+// API key and format from openweathermap.org
+const API_KEY = '83a5f58152c31776f45da4292b4ac3b4';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+    // we assume all searches are with country code 'us'
+    const url = `${ROOT_URL}&q=${city},us`;
+
+    // note, axios.get returns a promise
+    const request = axios.get(url);
+
+    return {
+        type: FETCH_WEATHER,
+        payload: request
+    };
+}
